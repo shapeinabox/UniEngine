10648a41a0cd909083004324cadc79c9b1152706
Alberto Bonino <47081957+shapeinabox@users.noreply.github.com>
2024-05-07 18:31:42 +0200

    fixes available times

--- modules/flightControl/utils/helpers/getAvailableExpeditionTimes.helper.php
+++ modules/flightControl/utils/helpers/getAvailableExpeditionTimes.helper.php
@@ -5,7 +5,7 @@ namespace UniEngine\Engine\Modules\FlightControl\Utils\Helpers;
 function getAvailableExpeditionTimes()
 {
 //    $availableOptions = range(0.1, 1, 0.1);
-    $availableOptions = [0.30, 0, 5, 0.7, 1, 2, 3, 4, 5];
+    $availableOptions = [0.3, 0.5, 0.7, 1, 2, 3, 4, 5];
 
     return $availableOptions;
 }
