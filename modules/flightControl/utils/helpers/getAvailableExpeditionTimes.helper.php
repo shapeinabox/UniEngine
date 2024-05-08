9a875c083284b4391957120d84f1f0d265b6dd7e
Alberto Bonino <47081957+shapeinabox@users.noreply.github.com>
2024-05-08 16:54:09 +0200

    minimum 1hr expedition time, formats resources in message

--- modules/flightControl/utils/helpers/getAvailableExpeditionTimes.helper.php
+++ modules/flightControl/utils/helpers/getAvailableExpeditionTimes.helper.php
@@ -5,7 +5,7 @@ namespace UniEngine\Engine\Modules\FlightControl\Utils\Helpers;
 function getAvailableExpeditionTimes()
 {
 //    $availableOptions = range(0.1, 1, 0.1);
-    $availableOptions = [0.6, 1, 2, 3, 4];
+    $availableOptions = [1, 2, 3, 4];
 
     return $availableOptions;
 }
